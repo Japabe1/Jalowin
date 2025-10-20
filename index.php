21addecb17f66ba9b31f6e26bc5fb0f9a88ffa5f
JuanAntonioMedac <jcl0035@alu.medac.es>
2025-10-20 18:48:15 +0200

    Add initial PHP files and remove index.html

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'funciones.php';
+if(isLoggedIn()){
+    header('Location: home.php');
+    exit();
+}
+$error='';
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $username=$_POST['username']??'';
+    $password=$_POST['password']??'';
+
+    if (verifyLogin($username,$password)){
+        header('Location: home.php');
+        exit();
+    }else {
+        $error='Usuario o contraseña incorrecto';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
