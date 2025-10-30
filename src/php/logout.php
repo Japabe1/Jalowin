d2c95b0d1133865c7f69c13277d1e25383f7d9d8
= <=>
2025-10-30 21:00:11 +0100

    logout.php

---
+++ src/php/logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Destruir la sesión
+session_unset();
+session_destroy();
+
+// Borrar cookie
+setcookie("usuario", "", time() - 3600, "/");
+
+header("Location: ./index.php");
+exit;
+?>
