cbc8e1dfa16c2e7c738442dcf7cc46615b4fdbfc
JuanAntonioMedac <jcl0035@alu.medac.es>
2025-11-07 20:04:53 +0100

    home

--- assets/script/login.js
+++ assets/script/login.js
@@ -1,3 +1,11 @@
 const flipCard = document.querySelector(".flip-card");
-document.getElementById("btnShowRegister").onclick = () => flipCard.classList.add("active");
-document.getElementById("btnShowLogin").onclick = () => flipCard.classList.remove("active");
+document.getElementById("btnShowRegister").onclick = () => {
+    const alertElement = document.querySelector('.alert');
+    if (alertElement) alertElement.style.display = 'none';
+    flipCard.classList.add("active");
+};
+document.getElementById("btnShowLogin").onclick = () => {
+    const alertElement = document.querySelector('.alert');
+    if (alertElement) alertElement.style.display = 'none';
+    flipCard.classList.remove("active");
+};
